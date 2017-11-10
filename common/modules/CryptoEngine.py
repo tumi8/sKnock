ff115016c2a11c5622b352c4875279e8f925c545
Sree Harsha Totakura <sreeharsha@totakura.in>
2017-11-10 18:25:55 +0100

    Use cryptography instead of M2crypto

--- common/modules/CryptoEngine.py
+++ common/modules/CryptoEngine.py
@@ -1,17 +1,24 @@
-import hashlib
 import logging
-
-from M2Crypto import *
-#from hkdf import hkdf_expand, hkdf_extract
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
-from Utils import *
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.padding import PKCS7
+from cryptography.hazmat.primitives.serialization import (
+    load_pem_private_key,
+    load_pem_public_key,
+    load_der_public_key,
+    Encoding,
+    PublicFormat)
 
 SIGNATURE_SIZE = 73
 
 LOG = logging.getLogger(__name__)
 
+_TAG_KNOCK = b"knock"
+_TAG_IV = b"iv0AES128CBC"
+_BLOCKSIZE = 128
 class CryptoEngine:
 
     def __init__(self, privKey):
@@ -19,51 +26,50 @@ class CryptoEngine:
 
     def encryptWithECIES(self, message, pubKey):
         LOG.debug("Generating ECC ephermal key...")
-        ephermal = EC.gen_params(EC.NID_X9_62_prime256v1)
-        ephermal.gen_key()
-
+        ephermal = ec.generate_private_key(ec.SECP256R1(), # NIST P-256 Curve
+                                      default_backend())
         LOG.debug("Deriving AES symmetric key...")
-        ecdhSecret = ephermal.compute_dh_key(pubKey)
-        aesKey = self._hkdf(ecdhSecret)
-
+        ecdhSecret = ephermal.exchange(ec.ECDH(), pubKey)
+        aesKey = self._hkdf(_TAG_KNOCK, ecdhSecret)
         LOG.debug("Encrypting with AES...")
-        encrypt = EVP.Cipher(alg='aes_128_cbc', key=aesKey, iv = '\0' * 16, padding=1, op=1)
-        encryptedMessage = encrypt.update(message)
-        encryptedMessage += encrypt.final()
+        iv = self._hkdf(_TAG_IV, ecdhSecret)
+        padder = PKCS7(_BLOCKSIZE).padder()
+        padded_message = padder.update(message) + padder.finalize()
+        encryptor = Cipher(algorithms.AES(aesKey), modes.CBC(iv), default_backend()).encryptor()
+        encryptedMessage = encryptor.update(padded_message) + encryptor.finalize()
 
-        ephPubKeyBIO = BIO.MemoryBuffer()
-        ephermal.save_pub_key_bio(ephPubKeyBIO)
-        ephPubKey = convertPEMtoDER(ephPubKeyBIO.read_all())
-
+        # Serialize the ephemeral pub key in DER format
+        ephPubKey = ephermal.public_key().public_bytes(Encoding.DER, PublicFormat.SubjectPublicKeyInfo)
         return encryptedMessage, ephPubKey
 
     def decryptWithECIES(self, encryptedMessage):
         LOG.debug("Calculating decryption key...")
-        ephPubKey = encryptedMessage[-91:]
+        eph_pubkey_bytes = encryptedMessage[-91:]
+        ephPubKey = load_der_public_key(eph_pubkey_bytes, default_backend())
         encryptedMessage = encryptedMessage[0:-91]
-        ecdhSecret = self.privKey.compute_dh_key(EC.load_pub_key_bio(BIO.MemoryBuffer(convertDERtoPEM(ephPubKey))))
-        aesKey = self._hkdf(ecdhSecret)
-
+        ecdhSecret = self.privKey.exchange(ec.ECDH(), ephPubKey)
+        aesKey = self._hkdf(_TAG_KNOCK, ecdhSecret)
+        iv = self._hkdf(_TAG_IV, ecdhSecret)
         LOG.debug("Decrypting AES encrypted request...")
-        decrypt = EVP.Cipher(alg='aes_128_cbc', key=aesKey, iv = '\0' * 16, padding=1, op=0)
-        decryptedMessage = decrypt.update(encryptedMessage)
-        decryptedMessage += decrypt.final()
-
+        decryptor = Cipher(algorithms.AES(aesKey), modes.CBC(iv), default_backend()).decryptor()
+        decryptedMessage_padded = decryptor.update(encryptedMessage) + decryptor.finalize()
+        unpadder = PKCS7(_BLOCKSIZE).unpadder()
+        decryptedMessage = unpadder.update(decryptedMessage_padded) + unpadder.finalize()
         return decryptedMessage
 
-    def _hkdf(self, ecdhSecret):
+    def _hkdf(self, tag, ecdhSecret):
         hkdf = HKDF(algorithm=hashes.SHA256(),
-                    length=16,
+                    length=_BLOCKSIZE/8,
                     salt=b"54686579206c6976696e272069742075702061742074686520486f74656c2043616c69666f726e69610a576861742061206e6963652073757270726973652028776861742061206e696365207375727072697365290a4272696e6720796f757220616c69626973",
-                    info=b"knock",
+                    info=tag,
                     backend=default_backend())
         return hkdf.derive(ecdhSecret)
 
 
     @staticmethod
     def loadPrivKeyFromPEM(pem):
-        return EC.load_key_bio(BIO.MemoryBuffer(pem))
+        return load_pem_private_key(pem, None, default_backend())
 
     @staticmethod
     def loadPubKeyFromPEM(pem):
-        return EC.load_pub_key_bio(BIO.MemoryBuffer(pem))
+        return load_pem_public_key(pem, default_backend())
